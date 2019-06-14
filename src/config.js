5b26b79957459b9c60675ebab5be6ad6dc779d2e
Gabriel Neil <gabriel.neil@mercadolibre.com>
2019-06-13 21:51:14 -0300

    renamed identity_pool_id

--- src/config.js
+++ src/config.js
@@ -7,6 +7,6 @@ export default {
 		REGION: 'us-east-1',
 		USER_POOL_ID: 'us-east-1_vwGNEHsur',
 		APP_CLIENT_ID: '6c5evaa6himr6dvmjhaflifk87',
-		IDENTITY_POOL_ID: 'us-east-1:340508a3-ce27-4c2c-8462-e1b3ad9c78d2'
+		IDENTITY_POOL_ID: 'us-east-1:d748d33d-43f5-4cae-9ba3-2bec84923a83'
 	}
 };
