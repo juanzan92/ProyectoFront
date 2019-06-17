f886f335c45917e8ac5a1966d87a0e7de2e7d544
Gabriel Neil <gabriel.neil@mercadolibre.com>
2019-06-17 17:37:12 -0300

    moving methods

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 import Footer from '../../components/Footer';
 import NavBar from '../../components/NavBar';
 import TopBar from '../../components/TopBar';
-import { Auth } from 'aws-amplify';
 import { async } from 'q';
 import wrapper from '../../components/Wrapper';
 //servicio
@@ -26,65 +25,9 @@ class index extends React.Component {
   }
 
 
-  async signUp(user, password, email) {
-    Auth.signUp({
-      username: user, //parametrizar
-      password: password,
-      attributes: {
-        email: email
-      },
-      validationData: []  //optional
-    })
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
-  };
-
-  async signIn(user, password) {
-    try {
-      await Auth.signIn(user, password);
-      console.log(Auth.currentAuthenticatedUser());
-   } catch (e) {
-     alert(e.message);
-   }
-   };
-
-  async signOut() {
-    Auth.currentSession()
-    .catch(err => console.log(err));
-  };
-
-  async changePassword(oldPassword, newPassword) {
-    Auth.currentAuthenticatedUser()
-      .then(user => {
-        return Auth.changePassword(user, oldPassword, newPassword);
-      })
-      .catch(err => console.log(err));
-  };
-
-  async forgotPassword(user) {
-    Auth.forgotPassword(user)
-      .catch(err => console.log(err));
-  };
-
-
-  async verifyCurrentUserAttribute() {//recuperar cuenta
-    Auth.verifyCurrentUserAttribute('email')
-      .then(() => {
-        console.log('a verification mail is sent');
-      }).catch((e) => {
-        console.log('failed with error', e);
-      });
-  };
-
   render() {
     return (
       <div>
-        <div className="btn btn-success" onClick={this.signUp('testuser', 'password123', 'gabrielneil7@gmail.com')}> signUp</div>
-        <div className="btn btn-success" onClick={this.signIn('testuser', 'password123')}> signIn</div>
-        <div className="btn btn-success" onClick={this.signOut}> signOut</div>
-        <div className="btn btn-success" onClick={this.changePassword('password123', 'password111')}> changePassword</div>
-        <div className="btn btn-success" onClick={this.forgotPassword('testuser')}> forgotPassword</div>
-        <div className="btn btn-success" onClick={this.verifyCurrentUserAttribute}> verifyCurrentUserAttribute</div>
         {/* Body*/}
         {/* Off-Canvas Category Menu*/}
         <div className="offcanvas-container" id="shop-categories">
--- src/views/login/logController.jsx
+++ src/views/login/logController.jsx
@@ -8,7 +8,7 @@ import * as Ivar from './Ivar';
 import Login from './logInView';
 import SignIn from '../../components/SignIn';
 import SignUp from '../../components/SignUp';
-
+import { Auth } from 'aws-amplify';
 
 class loginController extends React.Component {
    constructor(props){
@@ -18,6 +18,56 @@ class loginController extends React.Component {
        
     }
    }
+
+   async signUp(user, password, email) {
+    Auth.signUp({
+      username: user, //parametrizar
+      password: password,
+      attributes: {
+        email: email
+      },
+      validationData: []  //optional
+    })
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  };
+
+  async signIn(user, password) {
+    try {
+      await Auth.signIn(user, password);
+      console.log(Auth.currentAuthenticatedUser());
+   } catch (e) {
+     alert(e.message);
+   }
+   };
+
+  async signOut() {
+    Auth.currentSession()
+    .catch(err => console.log(err));
+  };
+
+  async changePassword(oldPassword, newPassword) {
+    Auth.currentAuthenticatedUser()
+      .then(user => {
+        return Auth.changePassword(user, oldPassword, newPassword);
+      })
+      .catch(err => console.log(err));
+  };
+
+  async forgotPassword(user) {
+    Auth.forgotPassword(user)
+      .catch(err => console.log(err));
+  };
+
+
+  async verifyCurrentUserAttribute() {//recuperar cuenta
+    Auth.verifyCurrentUserAttribute('email')
+      .then(() => {
+        console.log('a verification mail is sent');
+      }).catch((e) => {
+        console.log('failed with error', e);
+      });
+  };
 
     cognitoPOC = () => {
         //la idea es que metas un JSON con los datos de form
@@ -35,6 +85,13 @@ class loginController extends React.Component {
         return (
             <>
                 <div>
+                <div className="btn btn-success" onClick={this.signUp('testuser', 'password123', 'gabrielneil7@gmail.com')}> signUp</div>
+        <div className="btn btn-success" onClick={this.signIn('testuser', 'password123')}> signIn</div>
+        <div className="btn btn-success" onClick={this.signOut}> signOut</div>
+        <div className="btn btn-success" onClick={this.changePassword('password123', 'password111')}> changePassword</div>
+        <div className="btn btn-success" onClick={this.forgotPassword('testuser')}> forgotPassword</div>
+        <div className="btn btn-success" onClick={this.verifyCurrentUserAttribute}> verifyCurrentUserAttribute</div>
+
                     ?isLogin
                     {/*<SignIn props={"isLoading:true"}/>*/}
                     <SignUp/>
