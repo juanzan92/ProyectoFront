bc03944d49b39358d75a53c38008d4a2087cfa02
lucasjdelrio <lucasjdr_97@hotmail.com>
2019-08-22 23:34:54 -0300

    Navbar render if current session

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React from 'react';
-import './App.scss';
+import React, { Component, Fragment } from "react";
+import "./App.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from './views/home/home';
-import Login from './views/login/logController';
-import CheckoutController from './views/checkout/checkoutController';
-import vip from './views/item/vip';
-import splash from './views/splash/splash';
-import SignUp from './components/SignUp';
-import SignIn from './components/SignIn';
-import ForgotPassword from './components/ForgotPassword';
+import Home from "./views/home/home";
+import Login from "./views/login/logController";
+import CheckoutController from "./views/checkout/checkoutController";
+import vip from "./views/item/vip";
+import splash from "./views/splash/splash";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import ForgotPassword from "./components/ForgotPassword";
 //import { Auth } from 'aws-amplify';
 //import awsmobile from './aws-exports';
 //Amplify.configure(awsmobile);
-import { withAuthenticator } from 'aws-amplify-react';
-import Amplify from 'aws-amplify';
-import awsmobile from './aws-exports';
+//<<import { withAuthenticator } from 'aws-amplify-react';
+import { Auth } from "aws-amplify";
+//import awsmobile from './aws-exports';
 // Get the aws resources configuration parameters
 //import awsconfig from './aws-exports'; // if you are using Amplify CLI
+//Amplify.configure(awsmobile);
 
-Amplify.configure(awsmobile);
-
-
-function App() {
-  return (
-    <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/checkout" component={CheckoutController}/>
-      <Route exact path="/signin" component={SignIn} />
-      <Route exact path="/signup" component={SignUp} />
-      <Route exact path="/forgot-password" component={ForgotPassword} />
-      <Route exact path="/vip" component={vip}/>
-      <Route path="/splash" component={splash}/>
-    </Router>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/checkout" component={CheckoutController} />
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/forgot-password" component={ForgotPassword} />
+        <Route exact path="/vip" component={vip} />
+        <Route path="/splash" component={splash} />
+      </Router>
+    );
+  }
 }
-
-export default withAuthenticator(App, true);
-//export default App;
+//export default withAuthenticator(App, true);
+export default App;
