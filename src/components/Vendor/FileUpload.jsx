47a58c71f281749bec24ea9dd42e71e8e3777c9e
lucasjdelrio <lucasjdr_97@hotmail.com>
2019-09-27 00:26:35 -0300

    Final inputs handle

--- src/components/Vendor/FileUpload.jsx
+++ src/components/Vendor/FileUpload.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import wrapper from "../Wrapper";
+import { Auth } from "aws-amplify";
 import axios from 'axios';
 import { Link } from "react-router-dom";
 import { number } from "prop-types";
@@ -9,22 +10,40 @@ class FileUpload extends React.Component {
     super(props);
 
     this.state = {
-      title: "",
-      category: "",
-      sellerId: "",
-      price: null,
-      inDiscount: false,
-      initialStock: null,
-      description: "",
-      descriptionShort: "",
-      pictures: [],
-      attributes: [],
-      tags: [],
-      dateCreated: null,      
+      sellerId: this.currentActiveUser(),
+      categories: null,
+      category: '',
+      title: '',
+      marca: '',
+      modelo: '',
+      color: '',
+      initial_stock: null,
+      initial_price: null,
+      description_short: '',
+      description: '',
       files: null,
+
+      pictures: [],
+
+      attributes: [],
+      
+      date_created: this.getDate(),
+      date_finished: null,
+
+      in_discount: false,
+      actual_price: null,
+      stock: null,
+      status: null,
+      tags: null,
+      
       url: '',
-      urlx: []
+      urlx: [],
+      
+      isAuthenticated: false
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   //funcion que reciba todas las llamadas del promise all
@@ -34,38 +53,87 @@ class FileUpload extends React.Component {
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     var yyyy = today.getFullYear();
-    
     today = dd + '/' + mm + '/' + yyyy;
     return today
   }
 
-  componentDidMount() {
-    this.setState({dateCreated: this.getDate()});
+  changeDateFormat(inputDate){  // expects Y-m-d
+    var newdate = inputDate.split("-").reverse().join("/");
+    return newdate
   }
 
-  assignInputValue(target){
-    if (target.type === 'email'){
-      if (target.name!=='userEmail') {
-        document.getElementById('userEmailConf').pattern = this.state.userEmail;
+  currentActiveUser() {
+    Auth.currentAuthenticatedUser({})
+      .then(user => {
+        this.setState({
+          sellerId: user.username.toLowerCase()
+        });
+        console.log(Auth.currentAuthenticatedUser());
+        console.log(JSON.stringify(this.state.sellerId));
+      })
+      .catch(err => console.log(err));
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+  
+  async componentDidMount() {
+    try {
+      if (await Auth.currentSession()) {
+        this.userHasAuthenticated(true);
       }
-      return target.value.toLowerCase(); 
-    }
-    if (target.type === 'password'){
-      if (target.name!=='userPw') {
-        document.getElementById('userPwConf').pattern = this.state.userPw;
+    } catch (e) {
+      if (e !== "No current user") {
+        alert(e);
       }
-      return target.value; 
-    }
-    if (target.type === 'text'){
-      if (target.name!=='userName') {
-        return target.value.toUpperCase();
-      }
-      return target.value.toLowerCase();
     }
 
-    if(target.type === 'select-one'){
-      return target.value.toLowerCase();
+    this.setState({dateCreated: this.getDate()});
+    this.fetchCategories();
+  }
+
+  handleAttribChange(marca, modelo, color) { 
+ 
+      let brand = {};
+      brand.id = 'Marca';                     
+      brand.value = marca;
+      this.state.attributes.push(brand);
+
+      let model = {};
+      model.id = 'Modelo';                     
+      model.value = modelo;
+      this.state.attributes.push(model);
+
+      let colour = {};
+      colour.id = 'Color';                     
+      colour.value = color;
+      this.state.attributes.push(colour);
+
+      /*
+      this.setState({
+        attributes: [...this.state.attributes, ...marca]
+      })
+      
     }
+    if (target.name=='modelo') {
+
+      let modelo = {};
+      modelo.id = 'Modelo';                     
+      modelo.value = value;
+      this.setState(prevState => ({
+        attributes: [...prevState.attributes, modelo]
+      }))
+      
+      this.setState({
+        attributes: [...this.state.attributes, modelo]
+      })
+
+      this.setState(prevState => ({
+        attributes: [...prevState.attributes, modelo]
+      }))
+      */
+
   }
 
   handleChange(event) { 
@@ -78,27 +146,63 @@ class FileUpload extends React.Component {
     });
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    this.signUp(this.state.userName, this.state.userPw, 
-      this.state.userEmail, this.state.userRol, this.state.userNombres, 
-      this.state.userApellidos, this.state.userDni, this.state.userPhone,
-      this.state.userAddCalle, this.state.userAddNum, this.state.userAddCp);
-      var confirm = window.confirm("Se ha enviado un enlace de verificación a su casilla de correo.\n¡Confirme su usuario e inicie sesión!");
-      if (confirm == true) {
-        window.location.href = "/signin";
-      } else {
-        window.location.href = "/";
+  assignInputValue(target){
+    if (target.type === 'text'){
+      return target.value.toUpperCase();
+    }
+    if(target.type === 'select-one'){
+      return target.value.toLowerCase();
+    }   
+    if(target.type === 'date'){
+      return this.changeDateFormat(target.value);
+    }
+    else{
+        return target.value
+    } 
+  
+    
+    /*
+    if (target.type === 'text'){
+      if (target.name!=='userName') {
+        return target.value.toUpperCase();
       }
+      return target.value.toLowerCase();
+    }
+    
+
+
+    if(target.type === 'select-one'){
+      return target.value.toLowerCase();
+    }
+    if (target.type === 'text'){
+      return target.value.toUpperCase();
+    }
+    */
   }
 
-  getCategories() {
-    const url = "http://localhost:8080/catalog/categories/get_all";
-    fetch(url, {
+  handleSubmit(event) {
+    event.preventDefault();
+    this.handleAttribChange(this.state.brand, this.state.model, this.state.colour);
+    console.log(JSON.stringify(this.state));
+    console.log(JSON.stringify(this.state.attributes))
+  }
+
+  getCategories(){
+    fetch('http://localhost:8080/catalog/categories/get_all', {
       method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
+      mode: 'no-cors'
+    })
+    .then(data => data.json())
+    .then((data) => { this.setState({ categories: data }) 
+    .catch(e => console.log(e));
+    }); 
+  }
+  
+  fetchCategories() {
+    const url =
+      "http://localhost:8080/catalog/categories/get_all";
+    fetch(url, {
+      method: "GET"
     })
       .then(response => {
         return response.json();
@@ -106,8 +210,7 @@ class FileUpload extends React.Component {
       .then(myJson => {
         console.log(myJson);
         this.setState({
-          categories: myJson,
-          isLoading: false
+          categories: myJson
         });
       })
       .catch(e => console.log(e));
@@ -159,7 +262,6 @@ class FileUpload extends React.Component {
 
   handleFileUpload = (event) => {
     this.setState({files: event.target.files});
-    console.log(this.state.dateCreated)
   }
 
   render() {
@@ -167,17 +269,20 @@ class FileUpload extends React.Component {
       <>
       <div className="row padding-top-3x padding-bottom-3x">
           <div className="login-box col-md-6 offset-3">
-            <form onSubmit={this.submitFile}>
+            <form onSubmit={this.handleSubmit}>
 
             <div className="form-group row">
                   <label className="col-12" htmlFor="category">Categoría</label>
                   <div className="col-8">
-                    <select className="form-control" id="category" placeholder="Seleccione categorías">
-                      <option value="Dallas">Dallas</option>
-                      <option value="Houston">Houston</option>
-                      <option value="Los Angeles">Los Angeles</option>
-                      <option value="Miami">Miami</option>
-                      <option value="New York">New York</option>
+                    <select className="form-control" name="category" 
+                        placeholder="Seleccione categoría"
+                        value={this.state.category} 
+                        onChange={this.handleChange}>
+                      <option value="dallas">Dallas</option>
+                      <option value="houston">Houston</option>
+                      <option value="angeles" selected>Los Angeles</option>
+                      <option value="miami">Miami</option>
+                      <option value="newyork">New York</option>
                     </select>
                     <small className="form-text text-muted">
                       Indicá una categoría para tu oportunidad.
@@ -188,7 +293,12 @@ class FileUpload extends React.Component {
               <div className="form-group row">
                   <label className="col-12" htmlFor="title">Título</label>
                   <div className="col-8">
-                    <input className="form-control" type="text" id="title" placeholder="Ej: Termo Stanley Classic 750ml"/>
+                    <input className="form-control" type="text" name="title" 
+                        placeholder="Ej: Termo Stanley Adventure 750ml"
+                        pattern="[A-Z0-9Ñ\s]{3,40}"
+                        value={this.state.title}
+                        onChange={this.handleChange}
+                        requiered/>
                     <small className="form-text text-muted">
                       Comenzá describiendo tu oportunidad. Indicá producto, marca y modelo.
                     </small>
@@ -196,10 +306,105 @@ class FileUpload extends React.Component {
               </div>
 
               <div className="form-group row">
-                  <label className="col-12" htmlFor="title">Imágenes</label>
+                  <div className="col-4">
+                      <div className="form-group input-group">
+                        <label htmlFor="marca">Marca</label>
+                        <input className="form-control" type="text" name="marca"  
+                            placeholder="Stanley"
+                            pattern="^[a-zA-Z0-9_-]{2,40}"
+                            title="Alfanumérico. Entre 2-40 caracteres."
+                            value={this.state.marca}
+                            onChange={this.handleChange} 
+                            required/>
+                      </div>
+                  </div>
+                  <div className="col-4">
+                      <div className="form-group input-group">
+                        <label htmlFor="modelo">Modelo</label>
+                        <input className="form-control" type="text" name="modelo"  
+                            placeholder="Adventure 750ml"
+                            pattern="[A-Z0-9Ñ\s]{3,40}"
+                            title="Alfanumérico. Entre 2-60 caracteres."
+                            value={this.state.modelo}
+                            onChange={this.handleChange}  
+                            required/>
+                      </div>
+                  </div>
+                  <div className="col-4">
+                      <div className="form-group input-group">
+                        <label htmlFor="color">Color</label>
+                        <input className="form-control" type="text" name="color" 
+                            placeholder="Azul"
+                            pattern="^[A-Z]{3,15}"
+                            title="Sólo texto. Entre 2-15 caracteres."
+                            value={this.state.color}
+                            onChange={this.handleChange}  
+                            required/>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="form-group row">
+                  <div className="col-4">
+                      <div className="form-group input-group">
+                        <label htmlFor="initial_stock">Unidades</label>
+                        <input className="form-control" type="text" name="initial_stock"  
+                            placeholder="Ej: 100"
+                            pattern="[0-9]{1,5}"
+                            title="Alfanumérico. Entre 2-5 caracteres."
+                            value={this.state.initial_stock}
+                            onChange={this.handleChange} 
+                            required/>
+                      </div>
+                  </div>
+                  <div className="col-4">
+                      <div className="form-group input-group">
+                        <label htmlFor="initial_price">Precio Unitario</label>
+                        <input className="form-control" type="text" name="initial_price"  
+                            placeholder="$"
+                            pattern="[0-9]+(\\.[0-9][0-9]?)?"
+                            title="Numérico. Admite 2 decimales."
+                            value={this.state.initial_price}
+                            onChange={this.handleChange}  
+                            required/>
+                      </div>
+                  </div>
+                  <div className="col-4">
+                    <div className="form-group input-group">
+                      <label className="col-12" htmlFor="date_finished">Fecha de Cierre</label>
+                      <input className="form-control" type="date" name="date_finished" 
+                            onChange={this.handleChange}/>
+                    </div>
+                  </div>
+              </div>
+
+              <div className="form-group row">
+                <label className="col-2 col-form-label" htmlFor="description_short">Resumen</label>
+                <div className="col-10">
+                  <textarea className="form-control" name="description_short" rows="3" 
+                        placeholder="Resumí lo esencial de tu oportunidad!"
+                        value={this.state.description_short}
+                        onChange={this.handleChange}/>
+                </div>
+              </div>
+
+              <div className="form-group row">
+                <label className="col-2 col-form-label" htmlFor="description">Descripción</label>
+                <div className="col-10">
+                  <textarea className="form-control" name="description" rows="7" 
+                        placeholder="Describí detalladamente tu oportunidad!"
+                        value={this.state.description}
+                        onChange={this.handleChange}/>
+                </div>
+              </div>
+
+              <div className="form-group row">
+                  <label className="col-12" htmlFor="file_input">Imágenes</label>
                   <div className="col-8 padding-left:5px">
-                  <input className="custom-file" type="file" id="file-input" multiple onChange={this.handleFileUpload}/>
-                    <label className="custom-file-label" htmlFor="file-input">Choose files...</label>
+                    <input className="custom-file" type="file" name="file_input" 
+                          multiple 
+                          onChange={this.handleFileUpload}/>
+                    <label className="custom-file-label" htmlFor="file_input">Choose files...</label>
                   </div> 
               </div>
 
@@ -212,7 +417,7 @@ class FileUpload extends React.Component {
             </form>
           </div>
           <br/>
-          <label>{JSON.stringify(this.state.files)}</label>
+          <label>{JSON.stringify(this.state)}</label>
       </div>
       </>
     );
