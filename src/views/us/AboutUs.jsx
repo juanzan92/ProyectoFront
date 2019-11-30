93f4b53e70521e2baad7cee6abf8e214944160fc
lucasjdelrio <lucasjdr_97@hotmail.com>
2019-11-30 00:20:42 -0300

    adding AboutUs.jsx

---
+++ src/views/us/AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import wrapper from "../../components/Wrapper";
+
+class AboutUs extends React.Component {
+  render() {
+    var style = {
+      width: "100%",
+      maxWidth: "550px"
+    };
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default wrapper(AboutUs);
